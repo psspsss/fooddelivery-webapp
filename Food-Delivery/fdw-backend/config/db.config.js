a67dba257a0f4b262252e2d17dccc095edb3c7cc
Vinayak Majhi <vinayak.djp@gmail.com>
2025-07-03 13:01:32 +0530

    Update db.config.js

--- Food-Delivery/fdw-backend/config/db.config.js
+++ Food-Delivery/fdw-backend/config/db.config.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://bitansarkar088:rigenix2002@cluster0.ev3cf.mongodb.net/food-delivery"
+      ""
     )
     .then(() => console.log("DB Connected"));
 };
